a74de7d5feb8c75527fe038af5fed9b360e68359
Camilo <camiloa.mejia@udea.edu.co>
2020-07-21 19:18:36 -0500

    Test de fallos

--- src/resources/controller.js
+++ src/resources/controller.js
@@ -1,4 +1,4 @@
-const sayHello = () => ({message: 'Hello World!'});
+const sayHello = () => ({message: 'Cambios para fallo'});
 
 const sayHelloController = (req,res) => {
     const hello = sayHello()
