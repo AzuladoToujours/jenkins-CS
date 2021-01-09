c5d1f2b2bb8d56b257b7929612f35211a8c94bd4
Camilo <camiloa.mejia@udea.edu.co>
2021-01-08 19:11:32 -0500

    testing not deployment

--- src/resources/controller.js
+++ src/resources/controller.js
@@ -1,4 +1,4 @@
-const sayHello = () => ({message: 'Hello World!'});
+const sayHello = () => ({message: 'Hello World! v2'});
 
 const sayHelloController = (req,res) => {
     const hello = sayHello()
